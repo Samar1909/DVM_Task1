f1d2d11f0c5290acaf0d203e1864e9bff8e8d493
Simarjot <Simarjot1909@gmail.com>
2025-02-08 11:47:46 +0530

    Changed bus models to account for days and timings of buses

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,16 +1,39 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+day_dict = {
+    "MON" : 'Monday',
+    "TUE" : 'Tuesday',
+    "WED" : 'Wednesday',
+    "THU" : 'Thursday',
+    "FRI" : 'Friday',
+    "SAT" : 'Saturday',
+    "SUN" : 'Sunday'
+}
+
+class day(models.Model):
+    day_code = models.CharField(max_length=3, choices=day_dict, unique=True)
+
+    def __str__(self):
+        return self.day_code
+
 
 class bus(models.Model):
+    bus_number = models.IntegerField(default = 10000, blank=False, null=False, validators=[MaxValueValidator(99999), MinValueValidator(10000)])
+    name = models.CharField(default = 'bus_name', blank=False, null=False, max_length=50)
     city1 = models.CharField(max_length = 30)
     city2 = models.CharField(max_length = 30)
     seats_total = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
     seats_available = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
-    timing = models.DateTimeField()
-    
+    days = models.ManyToManyField(day)
+    time = models.TimeField(default = timezone.now(), blank=False, null=False)
+        
+    def __str__(self):
+        return self.name
+
     def seats(self):
         self.seats_available = self.seats_total
 
